ee5e8b05883336425f50c08b6320d643c69ed8c2
Bluenarysun <tee.emilond@gmail.com>
2022-09-27 22:52:50 +0700

    add fastify

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import fastifyCookie, { FastifyCookieOptions } from '@fastify/cookie';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.setGlobalPrefix('api');
+
+  await app.useGlobalPipes(new ValidationPipe());
+  await app.enableCors();
+
+  await app.register(fastifyCookie, {
+    secret: 'my-secret', // for cookies signature
+    parseOptions: {}, // options for parsing cookies
+  } as FastifyCookieOptions);
+
   await app.listen(3000);
 }
 bootstrap();
